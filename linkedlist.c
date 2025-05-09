aa21462f1ce54bf15140d21f811bbb9a795d8eff
FLL77 <s2221155882@ku.edu.kw>
2025-05-09 18:11:54 +0300

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -4,20 +4,34 @@
 struct Node{
     int data;
     struct Node* next;
-}
+};
 
-void insertatbeg(struct Node** head ,int data){
+void addbeg(int value, struct Node** head){
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* last = *head;
-    newNode->data = data;
+    
+    newNode->data = value;
     newNode->next = *head;
     *head = newNode;
+    
 }
 
-void insertatend(struct Node** head ,int data){
+void addafternode(int value, struct Node* prev){
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    nextNode->next = NULL;
+    
+    if(prev == NULL){
+        return;
+    }
+    
+    newNode->data = value;
+    newNode->next = prev->next;
+    prev->next = newNode;
+}
+
+void addlast(int value, struct Node** head){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* last = *head;
+    newNode->next = NULL;
+    newNode->data = value;
     
     if(*head == NULL){
         *head = newNode;
@@ -27,38 +41,26 @@ void insertatend(struct Node** head ,int data){
     while(last->next != NULL){
         last = last->next;
     }
-    
     last->next = newNode;
 }
 
-void insertAfter(struct Node* prev, int data){
-    if(prev == NULL){
-        printf("prev is NULL");
-        return;
-    }
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = prev->next;
-    prev->next = newNode;
-}
-
-void deleteByValue(struct Node** head, int data){
-    struct Node* temp = *head;
+void deletebyvalue(int value, struct Node **head){
+    struct Node* temp= *head;
     struct Node* prev = NULL;
     
-    if(temp != NULL && temp->data == data){
+    if(temp != NULL && temp->data == value){
         *head = temp->next;
         free(temp);
         return;
     }
     
-    while(temp != NULL && temp->data != data){
+    while(temp != NULL &&  temp->data != value){
         prev = temp;
         temp = temp->next;
     }
     
     if(temp == NULL){
-        printf("value was not found");
+        printf("value is not found\n");
         return;
     }
     
@@ -67,15 +69,29 @@ void deleteByValue(struct Node** head, int data){
 }
 
 void printlist(struct Node* head){
+    if(head == NULL){
+        printf("list is empty\n");
+        return;
+    }
     while(head != NULL){
         printf("%d -> ", head->data);
-        head = head->data;
+        head = head->next;
     }
-    printf("NULL\n");
+    printf("NULL");
 }
 
-int main()
-{
+int main() {
+    struct Node* head = NULL;
+
+    addbeg(10, &head);         // List: 10
+    addbeg(20, &head);         // List: 20 -> 10
+    addlast(5, &head);         // List: 20 -> 10 -> 5
+    addafternode(15, head);    // List: 20 -> 15 -> 10 -> 5
+    deletebyvalue(10, &head);  // List: 20 -> 15 -> 5
+    deletebyvalue(100, &head); // Should print "value is not found"
+
+    printlist(head);  // Expected output: 20 -> 15 -> 5 -> NULL
 
     return 0;
 }
+
